5c99bf82db6d788c0f671802eb9c10be95417104
Nanxi <knx1029@gmail.com>
2017-10-22 18:09:11 -0700

    support edit photos + albums

--- RoadTripPlanner/Utils.swift
+++ RoadTripPlanner/Utils.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import Parse
+import UIKit
 
 class Utils {
     class func formatDate(date: Date) -> String {
@@ -15,5 +17,24 @@ class Utils {
         let month = calendar.component(.month, from: date)
         let day = calendar.component(.day, from: date)
         return "\(month)/\(day)/\(year)"
+    }
+    
+    class func roundImageCorner(image: UIImageView) {
+        image.layer.cornerRadius = image.frame.height / 2
+    }
+    
+    class func imageToFile(image: UIImage) -> PFFile? {
+        let data = UIImageJPEGRepresentation(image, 0.7)
+        return PFFile(data: data!)
+    }
+    
+    class func fileToImage(file: PFFile, callback: @escaping (UIImage) -> Void) {
+        file.getDataInBackground(block: { (data, error) -> Void in
+            if (error == nil) {
+                if let data = data {
+                    callback(UIImage(data: data)!)
+                }
+            }
+        })
     }
 }
